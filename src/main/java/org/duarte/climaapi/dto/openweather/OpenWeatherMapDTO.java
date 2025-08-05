cb78b928533cdb48664c28760f57a9527448041f
rduarte <duarterafael533@gmail.com>
2025-08-05 08:57:19 -0300

    add DTO classes

--- src/main/java/org/duarte/climaapi/dto/openweather/OpenWeatherMapDTO.java
+++ src/main/java/org/duarte/climaapi/dto/openweather/OpenWeatherMapDTO.java
@@ -1,4 +1,17 @@
 package org.duarte.climaapi.dto.openweather;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import java.util.List;
+
+@Data
 public class OpenWeatherMapDTO {
+    @JsonProperty("coord")
+    private CoordDTO coordinates;
+
+    private List<WeatherInfoDTO> weather;
+    private MainInfoDTO main;
+    private WindDTO wind;
+    private String name; //nome da cidade
+    private Long id;
 }
